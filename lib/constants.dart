84524f2581fdd9d8a687e6dfc419d0542b55fedb
Akhil Raj <akhil.raj@appsteamtechnologies.com>
2025-12-01 20:43:29 +0530

    build 1.0.2

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,6 +1,6 @@
 /// API base URL for backend requests
 // const String apiBaseUrl = 'http://localhost:3010/';
 // const String apiBaseUrl = 'http://10.0.2.2:3010/';
-const String apiBaseUrl = 'http://192.168.1.183:3010/';
+// const String apiBaseUrl = 'http://192.168.1.183:3010/';
 
-// const String apiBaseUrl = 'https://vps-24bb0d6f.vps.ovh.ca:5031/';
+const String apiBaseUrl = 'https://vps-24bb0d6f.vps.ovh.ca:5031/';
